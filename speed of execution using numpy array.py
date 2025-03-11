b3d5a26138a85eff1cba3c9bb5a55bfb37c954ed
ksbhavanaa <ksbhavana28@gmail.com>
2025-03-11 19:08:35 +0530

    Add files via upload

---
+++ speed of execution using numpy array.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import numpy as np
+import time
+size=10**6
+python_list=list(range(size))
+numpy_array=np.arange(size)
+start_time=time.time()
+python_list_result=[x+1 for x in python_list]
+end_time=time.time()
+python_list_time=end_time-start_time
+start_time=time.time()
+numpy_array_result=numpy_array+1
+end_time=time.time()
+numpy_array_time=end_time-start_time
+print(f"python list time : {python_list_time:.6f}seconds")
+print(f"numpy list time : {numpy_array_time:.6f}seconds")
+print(f"approximate time {python_list_time/numpy_array_time:.2f} times faster for this operation:")
+
+
+# In[ ]:
+
+
+
+
